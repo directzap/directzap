a19e949bcadcca85a01970fee4199e167ec50fe3
Victor Eduardo <39489857+victor18edu@users.noreply.github.com>
2021-10-26 16:45:47 -0300

    ..

--- resources/views/pages/integration/index.blade.php
+++ resources/views/pages/integration/index.blade.php
@@ -34,17 +34,17 @@
                 "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjE1YmM4NjdjZWMwYjQzMmI0YzA1ZDNmNjBiZDk2NGFmZmQ1YzA3YjBlNDE1N2ZkMjIzYmE3MTBmNDM3YzcxMGYwYjFmZDE0NjRmNDFmYzljIn0.eyJhdWQiOiI0NjI3MyIsImp0aSI6IjE1YmM4NjdjZWMwYjQzMmI0YzA1ZDNmNjBiZDk2NGFmZmQ1YzA3YjBlNDE1N2ZkMjIzYmE3MTBmNDM3YzcxMGYwYjFmZDE0NjRmNDFmYzljIiwiaWF0IjoxNjM1MjcyNTg3LCJuYmYiOjE2MzUyNzI1ODcsImV4cCI6MTY2NjgwODU4Nywic3ViIjoiMzM4OTQ0MiIsInNjb3BlcyI6W119.SMVyxbINeWtV9-AWvCloGGn74sBS4z-HmtpTa2k_Sqf9epyZ2u6qkTWjWhCIyF1ZS4ia-RpuEinpmFs9J3uRgp1C-HmuvH_JWTiFjIHNupWCKaJtq35Z8JWjE-4Q2rGtuXnxArdzG4f80r2Dnuadp-rFQbxVMfVgVWuay2uXBtse0WGsaPK8zrj855i8VP1kAN30NNgm1Bl9AHIUfNDjWibCD28oSqr1HxV9JKSrPFdNMEg5XXOvoH9H1B51urA93A-slSkCMC9vYU1PMTIQ8LqQxsVLWa9eI7iBNHvmZCBVPH7Xg_qRv3s-3E5ABz9H4u5LUOGxo3-M-lWYeFZOEv6Rq7vHTbmITBW8KI4oAoG2Rb2SEim-32sVmJ8kEQCyakAc35qG9jurqQXBG9f3p2vdd9Bceuz0jfJF-KGiSnROkWxNFbmB1YZU-fPtyiuA8JY2W6pbGj6xB4YDBwYs4f-QQgnIe5ECoHBgJuTkJxmuCWwWq2FM8JfQ3RQManDM41Lv1yoEBsqn6z9kWpC7Tvs1k5l3GHA40F6pCVoTMB26E9g4sY9nLx6ggIGh042EgUw2EOOc9SnZ_epeoKtpxKCfP5mp-jz7YcUoeqYc31GvjBxsf32gkJJr89iY8nldvZNbf_4acxuJU6Hz3NYJzCYEXvXNqgCW90IbOEM1xv4";
             $.ajax({
                 type: "GET",
-                dataType: "json",
+                dataType: "jsonp",
+                contentType: 'application/json',
+                crossDomain: true,
                 headers: {
+                    'Access-Control-Allow-Credentials': true,
                     'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'application/json',
-                    ' Access-Control-Allow-Methods': 'GET',
-                    'Access-Control-Allow-Headers': '*',
-                    'Access-Control-Max-Age': '86400',
+                    'Access-Control-Allow-Methods': 'GET',
+                    'Access-Control-Allow-Headers': 'application/json',
                     'Authorization': token,
-
                 },
-                url: 'https://ev.braip.com/api/v1/producers/products',
+                url: 'https://ev.braip.com/api/vendas',
                 success: function(response) {
                     console.log(response);;
                 }
