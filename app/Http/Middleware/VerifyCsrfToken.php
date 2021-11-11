0751c129131fd50520f11d67f52abb0c556d7710
Victor Eduardo <39489857+victor18edu@users.noreply.github.com>
2021-11-11 15:30:56 -0300

    except

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        "commissions/postback",
+        "api/commissions/postback",
     ];
 }
