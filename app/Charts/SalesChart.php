0dd5217de071ead520676c1932676dc27ba99f50
Victor Eduardo <39489857+victor18edu@users.noreply.github.com>
2022-03-07 17:48:43 -0300

    graficos

--- app/Charts/SalesChart.php
+++ app/Charts/SalesChart.php
@@ -55,9 +55,61 @@ class SalesChart
             }
         }
 
-        $set_labels = ['Aguardando Pagamento', 'Pagamento Aprovado', 'Cancelada', 'Chargeback', 'Devolvida', 'Em Análise', 'Estorno Pendente', 'Em Processamento', 'Parcialmente Pago', 'Pagamento Atrasado'];
+        $set_labels = [];
+        $set_values = [];
+        if ($aguardando_pagamento > 0) {
+            $set_labels[] = 'Aguardando Pagamento';
+            $set_values[] = $aguardando_pagamento;
+        }
+        if ($pagamento_aprovado > 0) {
+            
+            $set_labels[] = 'Aguardando Aprovado';
+            $set_values[] = $pagamento_aprovado;
+        }
+        if ($cancelada > 0) {
+            
+            $set_labels[] = 'Cancelada';
+            $set_values[] = $cancelada;
+        }
+        if ($chargeback > 0) {
+            $set_labels[] = 'Chargeback';
+            $set_values[] = $chargeback;
+            
+        }
+        if ($devolvida > 0) {
+            $set_labels[] = 'Devolvida';
+            $set_values[] = $devolvida;
+            
+        }
+        if ($em_analise > 0) {
+            $set_labels[] = 'Em Análise';
+            $set_values[] = $em_analise;
+            
+        }
+        if ($estorno_pendente > 0) {
+            
+            $set_labels[] = 'Estorno Pendente';
+            $set_values[] = $estorno_pendente;
+        }
+        if ($em_processamento > 0) {
+            
+            $set_labels[] = 'Em Processamento';
+            $set_values[] = $em_processamento;
+        }
+        if ($parcialmente_pago > 0) {
+            $set_labels[] = 'Parcialmente Pago';
+            $set_values[] = $parcialmente_pago;
+            
+        }
+        if ($pagamento_atrasado > 0) {
+            $set_labels[] = 'Aguardando Atrasado';
+            $set_values[] = $pagamento_atrasado;
+            
+        }
+
+        //$set_labels = ['Aguardando Pagamento', 'Pagamento Aprovado', 'Cancelada', 'Chargeback', 'Devolvida', 'Em Análise', 'Estorno Pendente', 'Em Processamento', 'Parcialmente Pago', 'Pagamento Atrasado'];
         //  $set_labels = ['Teste 1', 'Teste 2'];
-        $set_values = [$aguardando_pagamento, $pagamento_aprovado, $cancelada, $chargeback, $devolvida, $em_analise, $estorno_pendente, $em_processamento, $parcialmente_pago, $pagamento_atrasado];
+        //$set_values = [$aguardando_pagamento, $pagamento_aprovado, $cancelada, $chargeback, $devolvida, $em_analise, $estorno_pendente, $em_processamento, $parcialmente_pago, $pagamento_atrasado];
         return $this->chart->pieChart()
 
             ->setSubtitle('')
